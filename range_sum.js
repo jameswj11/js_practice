eb8af8e45cf575f5864ca8292b61698716d4366c
James <jameswj111@gmail.com>
2024-08-06 23:54:09 -0400

    fix spacing

--- range_sum.js
+++ range_sum.js
@@ -6,7 +6,6 @@ Implement the NumArray class:
 NumArray(int[] nums) Initializes the object with the integer array nums.
 int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
  
-
 Example 1:
 
 Input
@@ -21,7 +20,6 @@ numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
 numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
 numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
  
-
 Constraints:
 
 1 <= nums.length <= 104
